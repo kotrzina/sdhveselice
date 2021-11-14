9850c80ba57648efc69cf247848a1a4b721fe2bf
Tomas Kozak <kozak@talko.cz>
2021-11-14 16:38:08 +0100

    feat: add test for races

--- cypress/integration/walkthrough.spec.js
+++ cypress/integration/walkthrough.spec.js
@@ -1,3 +1,15 @@
+function testRace(linkName) {
+    cy.contains(linkName).click()
+    cy.wait(250)
+    cy.get('h1').then(($h1) => {
+        const t = $h1.text();
+        expect(t.length).to.be.at.least(10)
+
+    })
+    cy.contains('1.') // all pages contain first place
+    cy.go('back')
+}
+
 describe('walk', () => {
 
     it('test #1', () => {
@@ -6,6 +18,20 @@ describe('walk', () => {
         cy.get('h2 a').click()
         cy.get('h1').should('contain.text', 'Novinky')
         cy.get('h2').first().click()
+
+        // Races in Veselice
+        cy.get('.navbar-nav a').contains('Závody ve Veselici').click()
+        testRace("Závody ve Veselici 2010")
+        testRace("Závody ve Veselici 2009")
+        testRace("Závody ve Veselici 2008")
+        testRace("Závody ve Veselici 2007")
+        testRace("Závody ve Veselici 2006")
+        testRace("Závody ve Veselici 2005")
+        testRace("Závody ve Veselici 2004")
+        testRace("Závody ve Veselici 2003")
+        testRace("Závody ve Veselici 2002")
+        testRace("Závody ve Veselici 2001")
+        testRace("Závody ve Veselici 1996 - 2000")
 
         // Contacts
         cy.get('.navbar-nav a').contains('Kontakty').click()
